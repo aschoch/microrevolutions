b31343205b0731db41e0992a1a5b7779618f04b6
Alfredo Schoch <alfredoschoch@gmail.com>
2013-02-27 00:06:04 -0600

    Created rails application - MicroRevolutions

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microrevolutions::Application.config.secret_token = '3f4f4ec677286d1ea8639605dded091e14aeb38d10012f7abf83e9e2b52c92e51dd53ccb48c7e0205a373b0ed0672820344ad633a63b72be4e0aa071ed886b87'
